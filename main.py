996bb473116b82245f9dd2b3c99ae88892379d8b
bodoralex <bodaleaa@gmail.com>
2016-11-05 23:34:13 +0100

    késő van már

--- main.py
+++ main.py
@@ -1,11 +1,41 @@
 
 def display_inventory():
-    for i, y in inv
-    print(i, y)
+    print("Inventory: ")
+    for i in inv:
+        print(inv[i], i)
 
+def add_to_inventory(inventory, added_items):
+    for i in added_items:
+        if i in inventory:
+            inventory[i] += 1
+        else:
+            inventory[i] = 1
+    display_inventory()
 
+def print_table(order):
+    if order == '':
+        pass
+    if order in ['igen']:
+        print("farok")
+    '''if order == 'nem', 'ne', 'n':
+        print("pina")'''
+        
 inv = {'rope': 1, 'torch': 6, 'gold coin': 42, 'dagger': 1, 'arrow': 12}
+#dragon_loot = ['gold coin', 'dagger', 'gold coin', 'gold coin', 'ruby']
+#add_to_inventory(inv, dragon_loot)
+#print_table('i')
+valueLength, keyLength = 0,0
+for k, v in inv.items():
+    if valueLength < len(str(v)):
+        valueLength = len(str(v))
+    if keyLength < len(str(k)):
+        keyLength = len(str(k))
+print(valueLength, keyLength)
+valueLength, keyLength = valueLength+4, keyLength+4
+print(valueLength, keyLength)
+print("count".rjust(valueLength), "item name".rjust(keyLength))
+print("-"*(valueLength+keyLength+1))
+for key, value in inv.items():
+    print(str(value).rjust(valueLength), key.rjust(keyLength))
 
 
-
-display_inventory()
---
+++ miez.py
@@ -0,0 +1,5 @@
+for x in range(1, 11):
+    print(repr(x).rjust(2), repr(x*x).rjust(3), end=' ')
+    # Note use of 'end' on previous line
+    print(str(x*x*x).rjust(4))
+repr
